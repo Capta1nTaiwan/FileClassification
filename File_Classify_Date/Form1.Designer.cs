9f1579c7b6e7abdf11685a8e29089d54437ebead
Capta1nTaiwan <bightw24251@gmail.copm>
2022-08-22 15:42:38 +0800

    first commit

---
+++ File_Classify_Date/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace File_Classify_Date
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SelectPathBtn = new System.Windows.Forms.Button();
+            this.DoClassifyBtn = new System.Windows.Forms.Button();
+            this.tbxPath = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SelectPathBtn
+            // 
+            this.SelectPathBtn.Location = new System.Drawing.Point(37, 34);
+            this.SelectPathBtn.Name = "SelectPathBtn";
+            this.SelectPathBtn.Size = new System.Drawing.Size(152, 43);
+            this.SelectPathBtn.TabIndex = 1;
+            this.SelectPathBtn.Text = "選擇路徑";
+            this.SelectPathBtn.UseVisualStyleBackColor = true;
+            this.SelectPathBtn.Click += new System.EventHandler(this.SelectPathBtn_Click);
+            // 
+            // DoClassifyBtn
+            // 
+            this.DoClassifyBtn.Location = new System.Drawing.Point(37, 174);
+            this.DoClassifyBtn.Name = "DoClassifyBtn";
+            this.DoClassifyBtn.Size = new System.Drawing.Size(152, 43);
+            this.DoClassifyBtn.TabIndex = 2;
+            this.DoClassifyBtn.Text = "執行分類";
+            this.DoClassifyBtn.UseVisualStyleBackColor = true;
+            this.DoClassifyBtn.Click += new System.EventHandler(this.DoClassifyBtn_Click);
+            // 
+            // tbxPath
+            // 
+            this.tbxPath.Location = new System.Drawing.Point(37, 105);
+            this.tbxPath.Name = "tbxPath";
+            this.tbxPath.Size = new System.Drawing.Size(369, 33);
+            this.tbxPath.TabIndex = 3;
+            this.tbxPath.Text = "可將路徑複製於此";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 229);
+            this.Controls.Add(this.tbxPath);
+            this.Controls.Add(this.DoClassifyBtn);
+            this.Controls.Add(this.SelectPathBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button SelectPathBtn;
+        private System.Windows.Forms.Button DoClassifyBtn;
+        private System.Windows.Forms.TextBox tbxPath;
+    }
+}
+
---
+++ File_Classify_Date/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace File_Classify_Date
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectPathBtn_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tbxPath.Text = folderBrowserDialog1.SelectedPath;                
+            }
+        }
+
+        private void DoClassifyBtn_Click(object sender, EventArgs e)
+        {
+            string[] allfiles = new string[999999];
+            try
+            {
+                allfiles = Directory.GetFiles(tbxPath.Text, "*.*", SearchOption.AllDirectories);
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                MessageBox.Show("路徑錯誤，請檢查");
+                return;
+            }
+            
+            foreach (var file in allfiles)
+            {
+                FileInfo info = new FileInfo(file);
+                DateTime Date = info.LastWriteTime;
+                string DateOfFile = Date.Year + "-" + Date.Month + "-" + Date.Day;
+
+                if (!Directory.Exists(Path.Combine(tbxPath.Text, DateOfFile)))
+                {
+                    Directory.CreateDirectory(Path.Combine(tbxPath.Text, DateOfFile));
+                }
+
+                File.Move(file, Path.Combine(tbxPath.Text, DateOfFile, info.Name));
+            }
+            MessageBox.Show("分類完成");
+        }
+    }
+}
